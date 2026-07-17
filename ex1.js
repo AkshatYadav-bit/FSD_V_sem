5e16f9d20f630d897f067eb02a272781cc7510d0
Akshat <akshatyadavcv@gmail.com>
2026-07-17 14:20:18 +0530

    17-07 git exercise, created ex1.js file

---
+++ ex1.js
@@ -0,0 +1,21 @@
+// var a = 10;
+// var a = 20;
+// console.log("a = "+a);
+// var b = a++;
+// console.log("b = "+b);
+
+// function sum(a,b){
+//     return a+b;
+// }
+// console.log("sum = "+sum(2,3));
+
+const sum =(a,b) => a+b;
+console.log("sum = "+sum(2,4));
+console.log("sum = "+sum(21,34.4));
+
+
+
+
+
+
+
