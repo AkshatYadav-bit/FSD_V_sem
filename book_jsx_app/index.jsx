baebba74e6911c64290e9a4cae4c0be3186d522e
Akshat <akshatyadavcv@gmail.com>
2026-08-21 14:05:18 +0530

    jsx update

---
+++ book_jsx_app/index.jsx
@@ -0,0 +1,21 @@
+function Book(){
+    return(
+        <div>
+            <img src="book-image.jpg" width="100px" height="100px" />
+            <h2>Price:$475</h2>
+            <button>AddToCart</button>
+        </div>
+    )
+}
+function App(){
+    return(
+    <div>
+    <Book/>
+    <Book/>
+    <Book/>
+    </div>
+    )
+}
+const parent=document.getElementById("root");
+ReactDOM.createRoot(parent).render(<App/>);
+root.render(<App/>);
