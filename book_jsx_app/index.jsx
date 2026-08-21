98f756132ebd3dba253948ee6c3d9dd92536b4fc
Akshat <akshatyadavcv@gmail.com>
2026-08-21 14:10:00 +0530

    fixed jsx bookstore

--- book_jsx_app/index.jsx
+++ book_jsx_app/index.jsx
@@ -1,21 +1,23 @@
-function Book(){
-    return(
+function Book() {
+    return (
         <div>
             <img src="book-image.jpg" width="100px" height="100px" />
-            <h2>Price:$475</h2>
-            <button>AddToCart</button>
+            <h2>Price: $475</h2>
+            <button>Add To Cart</button>
         </div>
-    )
+    );
 }
-function App(){
-    return(
-    <div>
-    <Book/>
-    <Book/>
-    <Book/>
-    </div>
-    )
+
+function App() {
+    return (
+        <div>
+            <Book />
+            <Book />
+            <Book />
+        </div>
+    );
 }
-const parent=document.getElementById("root");
-ReactDOM.createRoot(parent).render(<App/>);
-root.render(<App/>);
+
+const parent = document.getElementById("root");
+
+ReactDOM.createRoot(parent).render(<App />);
